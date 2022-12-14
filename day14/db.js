6208ca564267477a68670aba95ddd103869bb007
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-12-14 17:32:13 +0530

    add

--- day14/db.js
+++ day14/db.js
@@ -3,7 +3,8 @@
 const mongoose = require("mongoose")
 mongoose.set('strictQuery', true);
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/web30")
+
+const connection = mongoose.connect("mongodb+srv://123vermaji:123vermaji@cluster0.cfhdsvk.mongodb.net/web30")
 
 
 const TodoSchema = new mongoose.Schema({
